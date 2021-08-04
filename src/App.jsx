b2db8f7be7a72d68cb7e78682ad39ebd032ad0d4
coffmanjrp <coffmanjrp@gmail.com>
2021-08-04 04:25:30 -0700

    Implement click event

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import { useState } from 'react';
 import './App.scss';
 
 function App() {
-  return <div className="App">Hello, world!</div>;
+  const [isActive, setIsActive] = useState(true);
+
+  return (
+    <div className="App">
+      <nav className={`nav${isActive ? ' active' : ''}`}>
+        <ul>
+          <li>
+            <a href="#!">Home</a>
+          </li>
+          <li>
+            <a href="#!">Works</a>
+          </li>
+          <li>
+            <a href="#!">About</a>
+          </li>
+          <li>
+            <a href="#!">Contact</a>
+          </li>
+        </ul>
+        <button className="icon" onClick={() => setIsActive((prev) => !prev)}>
+          <div className="line line1"></div>
+          <div className="line line2"></div>
+        </button>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
